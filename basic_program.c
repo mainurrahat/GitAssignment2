71e29abf009ee51223a227e526569bf5b8d42f8c
mainurrahat <mainurrahat51@gmail.com>
2025-08-11 23:08:38 +0600

    Add fibonacci in experimental-branch

--- basic_program.c
+++ basic_program.c
@@ -5,11 +5,28 @@ int factorial(int n)
         return 1;
     return n * factorial(n - 1);
 }
+int fibonacci(int n)
+{
+    if (n <= 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    int a = 0, b = 1, c, i;
+    for (i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
 
 int main()
 {
     printf("Hello, World!\n");
     int num = 5;
     printf("Factorial of %d is %d\n", num, factorial(num));
+    int fib_num = 10;
+    printf("Fibonacci of %d is %d\n", fib_num, fibonacci(fib_num));
     return 0;
 }
